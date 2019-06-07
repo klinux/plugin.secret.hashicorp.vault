f27e658ad384780b761b7991651cc32cc0d827d4
Kritika Singh <kritika.singh1994@gmail.com>
2019-06-07 15:08:30 +0530

    Added tests.

--- src/main/java/com/example/secrets/models/SecretConfig.java
+++ src/main/java/com/example/secrets/models/SecretConfig.java
@@ -36,6 +36,10 @@ public class SecretConfig {
         return GSON.fromJson(requestBody, SecretConfig.class);
     }
 
+    public String getProperty() {
+        return property;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
